90f089a86ae2c72cfd06760af9819a7384cd7a28
JD <zelenuj@gmail.com>
2020-12-11 12:32:24 +0200

    CU-bb0u62 [in progress] Автоматичне встановлення валюти

--- install.php
+++ install.php
@@ -11,10 +11,23 @@ $languageData = [
 	'sort_order'    =>  0,
 	'status'    =>  1,
 ];
+$currency_data = [
+	'title' =>  'Гривня',
+	'code'  =>  'UAH',
+	'symbol_left'   =>  '',
+	'symbol_right'  =>  'грн',
+	'decimal_place' =>  2,
+	'status'    =>  1,
+];
 
 $this->load->model('localisation/language');
 $language = $this->model_localisation_language->getLanguageByCode('uk-ua');
-
 if (empty($language['name'])) {
 	$this->model_localisation_language->addLanguage($languageData);
+}
+
+$this->load->model('localisation/currency');
+$currency = $this->model_localisation_currency->getCurrencyByCode($currency_data['code']);
+if (empty($currency['title'])) {
+	$this->model_localisation_currency->addCurrency($currency_data);
 }
