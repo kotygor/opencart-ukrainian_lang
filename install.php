6f4d7de5d210c1f5205257a50532d04664b1aa18
JD <zelenuj@gmail.com>
2020-12-11 12:02:41 +0200

    CU-bb0u4v[in progress]Автоматичне встановлення мови в адмінці

---
+++ install.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Заготовка, сирий код
+ * jd todo Два випадки інсталяції 1-й на ще не встановлений сайт (немає БД), 2-й - коли вже є БД, і можна активувати
+ */
+
+$languageData = [
+	'name'  =>  "Українська",
+	'code'  =>  "uk-ua",
+	'locale'    =>  'ua_UA.UTF-8,ua_UA,ua_UA,ua',
+	'sort_order'    =>  0,
+	'status'    =>  1,
+];
+
+$this->load->model('localisation/language');
+$language = $this->model_localisation_language->getLanguageByCode('uk-ua');
+
+if (empty($language['name'])) {
+	$this->model_localisation_language->addLanguage($languageData);
+}
