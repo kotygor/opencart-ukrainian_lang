441811b50058449345421a0e9f24f3fa79fe2cca
JD <zelenuj@gmail.com>
2021-02-24 12:57:08 +0200

    text_no_account in common/header

--- upload/catalog/language/uk-ua/common/header.php
+++ upload/catalog/language/uk-ua/common/header.php
@@ -5,6 +5,7 @@ $_['text_wishlist'] = 'Закладки (%s)';
 $_['text_shopping_cart'] = 'Кошик';
 $_['text_category'] = 'Категорії';
 $_['text_account'] = 'Особистий кабінет';
+$_['text_no_account'] = 'Ще не зареєстровані?';
 $_['text_register'] = 'Реєстрація';
 $_['text_login'] = 'Авторизація';
 $_['text_order'] = 'Історія замовлень';
