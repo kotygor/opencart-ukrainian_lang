af5af836343d77f96a51f46888e2d31138ffe9e7
JD <zelenuj@gmail.com>
2021-03-24 13:21:30 +0200

    CU-fz7bx6[completed] Тексти на wishlist

--- upload/catalog/language/uk-ua/account/wishlist.php
+++ upload/catalog/language/uk-ua/account/wishlist.php
@@ -1,16 +1,16 @@
 <?php
 // Heading
-$_['heading_title'] = 'Мої закладки';
+$_['heading_title'] = 'Обрані';
 
 // Text
 $_['text_account'] = 'Особистий Кабінет';
 $_['text_instock'] = 'наявності';
-$_['text_wishlist'] = 'Закладки (%s)';
+$_['text_wishlist'] = 'Обрані (%s)';
 $_['text_login'] = 'Ви повинні <a href="%s \ ">увійти</a> або <a href="%s">створити обліковий запис</a>, щоб зберегти <a href="%s">%s</a> ваш <a href="%s">список закладок</a>!';
 $_['text_success'] = 'Ви додали <a href="%s">%s</a> <a href="%s">Закладки</a>!';
 $_['text_exists'] = '<a href="%s">%s</a> вже є <a href="%s">закладки</a>!';
 $_['text_remove'] = 'Список закладок успішно змінено!';
-$_['text_empty'] = 'Ваші закладки порожні';
+$_['text_empty'] = 'Список обраних товарів пустий!';
 
 // Column
 $_['column_image'] = 'Зображення';
